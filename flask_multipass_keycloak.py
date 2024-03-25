54f484545f8c2d0f162c3588ceeae496609f2dec
Tamas Mako <1412056+tomako@users.noreply.github.com>
2024-03-25 23:03:00 +0100

    Improve API error handling

--- flask_multipass_keycloak.py
+++ flask_multipass_keycloak.py
@@ -18,6 +18,7 @@ from flask_multipass.providers.authlib import AuthlibAuthProvider
 from flask_multipass.providers.authlib import AuthlibIdentityProvider
 from requests.auth import HTTPBasicAuth
 from requests.exceptions import RequestException
+from werkzeug.exceptions import BadRequest, NotFound
 
 CACHE_LONG_TTL = 86400 * 7
 CACHE_TTL = 1800
@@ -97,11 +98,10 @@ class KeycloakGroup(Group):
         if not group:
             return None
         with self.provider._get_api_session() as api_session:
-            url = urljoin(self.provider.keycloak_settings['realm_api_url'] + '/', f'groups/{group["id"]}/members')
-            response = api_session.get(url)
-            response.raise_for_status()
+            self.provider.logger.info('Requesting group members of "%s"', self.name)
+            members = self.provider._fetch_all(api_session, f'groups/{group["id"]}/members')
             identifier_field = self.provider.settings['identifier_field']
-            for member in response.json():
+            for member in members:
                 if identifier_field in member:
                     yield IdentityInfo(self.provider,
                                        member[identifier_field],
@@ -134,8 +134,9 @@ class KeycloakIdentityProvider(AuthlibIdentityProvider):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.cache = ExtendedCache(self.settings['cache'])
+        self.settings.setdefault('cache', None)
         self.settings.setdefault('logger_name', 'multipass.keycloak')
+        self.cache = ExtendedCache(self.settings['cache'])
         self.logger = logging.getLogger(self.settings['logger_name'])
 
     @property
@@ -155,49 +156,50 @@ class KeycloakIdentityProvider(AuthlibIdentityProvider):
 
     def search_groups(self, name, exact=False):
         with self._get_api_session() as api_session:
+            self.logger.info('Requesting matching groups ("%s")', name)
             params = {'search': name,
                       'exact': str(exact).lower(),
                       'populateHierarchy': 'false'}
-            url = urljoin(self.keycloak_settings['realm_api_url'] + '/', 'groups')
-            response = api_session.get(url, params=params)
-            for group in response.json():
+            groups = self._fetch_all(api_session,'groups', params=params)
+            for group in groups:
                 yield self.group_class(self, self.group_path_as_name(group['path']))
 
     def get_identity_groups(self, identifier):
         with self._get_api_session() as api_session:
             # query user details
-            params = {'email': identifier,
-                      'exact': 'true'}
-            url = urljoin(self.keycloak_settings['realm_api_url'] + '/', 'users')
-            response = api_session.get(url, params=params)
-            response.raise_for_status()
-            user_data = response.json()[0]
-            # query user's groups
-            url = urljoin(self.keycloak_settings['realm_api_url'] + '/',
-                          f'users/{user_data["id"]}/groups')
-            response = api_session.get(url)
-            if response.status_code == 404:
+            self.logger.info('Requesting data of user "%s"', identifier)
+            params = {'email': identifier, 'exact': 'true'}
+            users = self._fetch_all(api_session, 'users', params=params)
+            if not users:
                 return set()
-            response.raise_for_status()
-        return {self.group_class(self, group['name']) for group in response.json()}
+            # query user's groups
+            self.logger.info('Requesting groups of "%s"', identifier)
+            groups = self._fetch_all(api_session, f'users/{users[0]["id"]}/groups')
+        return {self.group_class(self, group['name']) for group in groups}
 
     @memoize_request
     def _get_group_data(self, name):
         # API allows searching groups only by name
-        _, _, real_group_name = name.rpartition(' > ')
+        _, real_group_name = name.rsplit(' > ', 1)
         group_path = self.group_name_as_path(name)
         params = {'search': real_group_name,
                   'exact': 'true',
                   'populateHierarchy': 'false'}
         with self._get_api_session() as api_session:
-            group_url = urljoin(self.keycloak_settings['realm_api_url'] + '/', 'groups')
-            response = api_session.get(group_url, params=params)
-            response.raise_for_status()
+            self.logger.info('Requesting data of group "%s"', name)
+            groups = self._fetch_all(api_session, 'groups', params=params)
             # Finding the group with the path
-            groups = [group for group in response.json() if group['path'] == group_path]
+            groups = [group for group in groups if group['path'] == group_path]
         if len(groups) != 1:
             return None
         return groups[0]
+
+    def _get_error_message(self, response):
+        data = response.json()
+        error_message = f'Keycloak API error: {response.status_code} - {data["error"]}'
+        if "error_description" in data:
+            error_message += f' - {data["error_description"]}'
+        return error_message
 
     @memoize_request
     def _get_api_session(self):
@@ -211,8 +213,23 @@ class KeycloakIdentityProvider(AuthlibIdentityProvider):
         data = {'username': self.keycloak_settings['username'],
                 'password': self.keycloak_settings['password'],
                 'grant_type': 'password'}
+        self.logger.info('Requesting access token')
         response = api_session.post(self.keycloak_settings['access_token_url'], auth=basic, data=data)
+        if response.status_code != 200:
+            error_message = self._get_error_message(response)
+            self.logger.error(f'{error_message} (URL: %s)', response.url)
+            raise BadRequest(error_message)
         api_token = response.json()['access_token']
         api_session.headers.update({'Authorization': f'Bearer {api_token}'})
+        self.logger.debug('Requested access token will expire in %s s', response.json()['expires_in'])
         self.cache.set(cache_key, api_token, response.json()['expires_in'] - 30)
         return api_session
+
+    def _fetch_all(self, api_session, endpoint, params=None):
+        url = urljoin(f'{self.keycloak_settings["realm_api_url"]}/', endpoint)
+        response = api_session.get(url, params=params)
+        if response.status_code != 200:
+            error_message = self._get_error_message(response)
+            self.logger.error(f'{error_message} (URL: %s)', response.url)
+            raise BadRequest(error_message)
+        return response.json()
